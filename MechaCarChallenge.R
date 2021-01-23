846702efc0f222fe916d52496e41bd65247170a2
stacychandisingh <71534644+stacychandisingh@users.noreply.github.com>
2021-01-23 15:55:50 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,5 +1,6 @@
 #Deliverable 1
 
+library(tidyverse)
 library(dplyr) #Load dplyr package
 
 mechacar_data <- read.csv("C:/Users/schan/OneDrive/R_Analysis/01_Demo/MechaCar_mpg.csv") #read in dataset
@@ -10,7 +11,6 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 #Deliverable 2
 suspension_coil <- read.csv("C:/Users/schan/OneDrive/R_Analysis/01_Demo/Suspension_Coil.csv") #read in dataset
 
-library(tidyverse)
 
 #summary statistics for PSI column
 total_summary <- suspension_coil %>% summarize(Mean = mean(PSI),
@@ -27,4 +27,20 @@ lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mea
                                                                              SD = sd(PSI), .groups="keep")
 
 lot_summary
-                                                                      
+
+
+#Deliverable 3
+
+#Statistical difference for all lots using t.test()
+t.test(suspension_coil$PSI, mu=1500, alternative="less")
+
+#t.test for Manufacturing Lot 1
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+
+#t.test for Manufacturing Lot 2
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+
+
+#t.test for Manufacturing Lot 3
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
+
